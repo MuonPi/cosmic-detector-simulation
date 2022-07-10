3803343fb96baba702e65f545320678bba8aff90
hangeza <zaunick@exp2.physik.uni-giessen.de>
2022-07-10 21:40:29 +0200

    added constant definitions for R3

--- algebra_types.h
+++ algebra_types.h
@@ -5,3 +5,12 @@
 
 typedef std::valarray<double> Point;
 typedef std::valarray<double> Vector;
+
+namespace R3 {
+    namespace Base {
+        const Vector X { 1., 0., 0. };
+        const Vector Y { 0., 1., 0. };
+        const Vector Z { 0., 0., 1. };
+    }
+}
+
