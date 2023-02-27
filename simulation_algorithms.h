0ff748677fb90a3f874021c61040201a587b0ac4
hangeza <zaunick@exp2.physik.uni-giessen.de>
2023-02-27 14:36:55 +0100

    updated doxygen-type comments

--- simulation_algorithms.h
+++ simulation_algorithms.h
@@ -22,7 +22,8 @@
  * @param theta_min the minimum zenith angle theta from which events are generated
  * @param theta_max the maximum zenith angle theta up to which events are generated
  * @param nr_bins The number of bins in the resulting acceptance histogram
- * @return A std::vector of Histogram objects containing the resulting distributions. Currently, only the angular acceptance (detection efficiency vs. theta) is provided
+ * @param histos A pointer to std::vector of Histogram objects containing the resulting distributions. Currently, only the angular acceptance (detection efficiency vs. theta) is provided
+ * @return nothing
  * @note In case of an error, the returned histogram vector is empty
  * @note The setup object must have the ref_detector iterator set to any valid detector it contains.
 */
@@ -55,13 +56,30 @@ std::array<std::array<double, THETA_BINS>, PHI_BINS> theta_phi_scan(const Detect
  * @param setup DetectorSetup object containing the detector definitions
  * @param gen random number generator (Mersenne-Twister engine of type std::mt19937)
  * @param nr_events The total number of tracks to be generated. This is not identical to the number of MC events (=events having a hit in the reference detector)
- * @param theta_max the maximum zenith angle theta up to which the result histograms are scaled. Note, that the tracks are generated within the full theta range (according to the underlaying distribution) independent of this parameter.
+ * @param histos A pointer to std::vector of Histogram objects containing the resulting distributions for theta ("theta_distribution"), accepted theta ("accepted_theta_distribution"), phi ("phi_distribution") and ("accepted_phi_distribution").
  * @param nr_bins The number of bins in the resulting histograms
+ * @param theta_max the maximum zenith angle theta up to which the result histograms are scaled. Note, that the tracks are generated within the full theta range (according to the underlaying distribution) independent of this parameter.
  * @param coinc_level the number of detectors which have to be crossed by a track in order to assert a coincidence event
- * @return A std::vector of Histogram objects containing the resulting distributions for theta ("theta_distribution"), accepted theta ("accepted_theta_distribution"), phi ("phi_distribution") and ("accepted_phi_distribution").
- * @note In case of an error, the returned histogram vector is empty
+ * @return A DataItem<double> object containing the total acceptance (ratio of detected hits to generated tracks) and it's statistical error.
+ * @note In case of an error, the histogram vector is not filled and the returned DataItem object is default initialized.
  * @note The setup object must have the ref_detector iterator set to any valid detector it contains.
 */
 DataItem<double> cosmic_simulation(const DetectorSetup& setup, std::mt19937& gen, std::size_t nr_events, std::vector<Histogram>* histos = nullptr, std::size_t nr_bins = 90, double theta_max = pi() / 2, int coinc_level = -1);
 
+/** @brief cosmic_simulation_detector_sweep
+ * This function does a full Monte-Carlo simulated evaluation of particle hits for a given detector setup for a scan over a given range of detector rotations around a given axis. Tracks are distributed as follows:
+ * - phi: uniformly distributed in -pi..pi
+ * - theta: distributed in 0..pi/2 according to the standard model for muons: ~cos^n(theta) with n=2
+ * @param setup DetectorSetup object containing the detector definitions
+ * @param gen random number generator (Mersenne-Twister engine of type std::mt19937)
+ * @param nr_events The total number of tracks to be generated. This is not identical to the number of MC events (=events having a hit in the reference detector)
+ * @param detector_rotation_axis A vector specifying the alignment of the rotation axis
+ * @param detector_min_angle the smallest rotation angle
+ * @param detector_min_angle the largest rotation angle
+ * @param nr_angles the number of steps in which the detector will be rotated between detector_min_angle and detector_max_angle
+ * @param coinc_level the number of detectors which have to be crossed by a track in order to assert a coincidence event
+ * @return A MeasurementVector<double> (aka std::vector<std::pair<DataItem<double>, DataItem<double>>) object containing the total acceptance (ratio of detected hits to generated tracks) and it's statistical error.
+ * @note In case of an error, the histogram vector is not filled and the returned MeasurementVector object is default initialized and empty.
+ * @note The setup object must have the ref_detector iterator set to any valid detector it contains.
+*/
 MeasurementVector<double, double> cosmic_simulation_detector_sweep(const DetectorSetup& setup, std::mt19937& gen, std::size_t nr_events, const Vector& detector_rotation_axis, double detector_min_angle, double detector_max_angle, std::size_t nr_angles, int coinc_level = -1);
